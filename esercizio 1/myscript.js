4bcb430069f27e8fe878e8eab8d4c84635c58081
elisabichiappini <elisabichiappini@gmail.com>
2023-11-12 18:07:35 +0100

    feat: condition access o no access

--- esercizio 1/myscript.js
+++ esercizio 1/myscript.js
@@ -9,16 +9,26 @@
 // -- SE VERO è nella lista di chi può accedere, allora stampo esito positivo;
 // -- ALTRIMENTI stampo accesso non consentito.
 
+// lista mail che hanno accesso
 const listaEmailUtenti = ['saradavanzo@gmail.com', 'robertochiappini@gmail.com', 'marirosso@gmail.com', 'eli@g.com', 'elisachiappini@gmail.com', 'stefanobasso@gmail.com', 'giuliavogliotti@gmail.com'];
 
-const emailUtente = prompt ('Inserisci la tua mail?');
+//stato iniziale
+let canAccess = false;
 
+//esecuzione della veririca di accesso
+const emailUtente = prompt ('Inserisci la tua mail?');
 
  for (let listControlEmail = 0; listControlEmail < listaEmailUtenti.length; listControlEmail++) {
     if (listaEmailUtenti[listControlEmail] === emailUtente) {
-        console.log(listControlEmail, 'mail valida per accesso');
-        break;
-    } else {
-       console.log(listControlEmail, 'mail non associata');
-  }
+      canAccess = true;
+      //interrompi ricerca se hai trovato corrispondenza
+      break;
+    }
 };
+
+//stato finale
+if (canAccess === false) {
+  console.log('Non puoi accedere');
+} else {
+  console.log('Puoi accedere');
+}
